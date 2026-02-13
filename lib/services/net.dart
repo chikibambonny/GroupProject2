ce748d9c781071be846b36c5c79ce0bdbf538d6b
middalew <middalew@gmail.com>
2026-02-13 22:52:24 +0200

    addedclient net.dart

--- lib/services/net.dart
+++ lib/services/net.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'dart:typed_data';
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -5,6 +6,8 @@ import 'package:sign_translate_app/services/config.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 class Net{
   late IO.Socket socket;
+  //chikibambony this is for you should replace it(Queue) with stream
+  late Queue<String> q= Queue<String>();
   void connect(){
     var host = Config.host;
     var port = Config.port;
@@ -21,6 +24,7 @@ class Net{
     socket.onConnect((_) {
       print('Connected to backend');
     });
+    recv();
   }
   void send(String command,List<int> bytes){
     String encoded = base64Encode(bytes);
@@ -34,5 +38,24 @@ class Net{
 
     socket.emit("video_event",jsonstr);
 
+  }
+  void recv() async{
+    socket.on('server_responce', (data){
+      final response = ServerResponse.fromJson(data);
+      q.add(response.message);
+    });
+  }
+}
+class ServerResponse {
+  final String message;
+  final bool success;
+
+  ServerResponse({required this.message, required this.success});
+
+  factory ServerResponse.fromJson(Map<String, dynamic> json) {
+    return ServerResponse(
+      message: json['message'] ?? '',
+      success: json['success'] ?? false,
+    );
   }
 }
